c6e75c73d9d6b034fd5e09a56ced322cc9b5cd5d
schoeggel <schoeggel@gmx.ch>
2015-10-18 22:26:32 +0200

    SchiessGerade wird gezeichnet. Einschlag wird angezeigt.

--- src/artilleryMain.m
+++ src/artilleryMain.m
@@ -39,38 +39,32 @@ RED = [.9, .3, .3];
 
 %% Bildschirm Starten
 %#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
-
-
 fig=createFigure(); %Figure erstellen und FigureHandler dazu erhalten
 
-
-
-
-%generate landscape, get landscape vertices, draw it
+%generate landscape, get landscape vertices
 [terrainshapeX,terrainshapeY] = genLandscape();
 c=terrainshapeY;
 
+%draw landscape
 patch(terrainshapeX,terrainshapeY, c,'EdgeColor','interp','MarkerFaceColor','flat');
 colormap(0.4*summer+0.4*flipud(pink)+0.1*flipud(winter));
 axis([1 300 0 300])
 
-
-
 % Panzerli: get Vertices
 [player1.shapeX, player1.shapeY] = genPlayer(-1);
-[player2.shapeX, player2.shapeY] = genPlayer(5);
+[player2.shapeX, player2.shapeY] = genPlayer( 5);
 
 %Player center-positions
-player1.posX = 15
-player2.posX = 290
-player1.posY = terrainshapeY(5) + 3
-player2.posY = terrainshapeY(62) +3 
+player1.posX = 15;
+player2.posX = 290;
+player1.posY = terrainshapeY(5) + 3;
+player2.posY = terrainshapeY(62) +3;
 
 % translate player polgon to player position
 player1.polygonX = player1.shapeX + player1.posX;
 player2.polygonX = player2.shapeX + player2.posX;
 player1.polygonY = player1.shapeY + player1.posY;
-player2.polygonY = player2.shapeY + player2.posY ;
+player2.polygonY = player2.shapeY + player2.posY;
 
 %draw tanks
 patch(player1.polygonX, player1.polygonY,'g')
@@ -79,19 +73,13 @@ patch(player2.polygonX, player2.polygonY,'y')
 %draw Powerbar
 updatePowerBar(0);
 
-
 % Enable Mouse interaction
 set(fig,'WindowButtonDownFcn',@mymousedowncallback)
 set(fig,'WindowButtonUpFcn',@mymouseupcallback)
 
-
-
-
 %Polling schleife. Falls Mouse down, zählt die Powerbar nach oben
-
-POWERTIMER=0
-%pause(1.5)
-GAMESTATE_PLAYERINPUT
+POWERTIMER=0;
+GAMESTATE_PLAYERINPUT=1;
 
 while GAMESTATE_PLAYERINPUT && strcmp(fig.Name,'Artillery')
     pause(0.01);
@@ -109,9 +97,7 @@ while GAMESTATE_PLAYERINPUT && strcmp(fig.Name,'Artillery')
         GAMESTATE_FIRE = false;
         GAMESTATE_PLAYERINPUT = true;
         POWERTIMER=0;
-        
-    end
-    
+    end   
 end
 
 
@@ -175,6 +161,50 @@ function [angle] = getAngle(playernr)
     
 end
 
+
+function [impactpos, hit] = gunfire (playernr, startY, fireAngle, power)
+% in ArtilleryMain integriert, weil beim Schuss auch noch das Terrain
+% verändert wird.
+
+playernr
+startY
+fireAngle
+power
+
+% alter comet zu testzwecken
+%t = 0:1/290:pi;
+%x = 15:1:290;
+%y = startY +220*sin((rand+2+(1-power))*t);
+%comet(x,y)
+
+gunposX=player1.posX-7;
+gunposY=player1.posY +3;
+t=[gunposX:1:300-gunposX];
+
+% Wurparabel : TODO geht noch nicht...
+%g=5.81;
+%power=power*1000
+%vx=cosd(fireAngle)*power
+%vy=sind(fireAngle)*power
+%x=t.*vx;
+%y=t.*vy-(g*t.^2)/2;
+
+% Stattessen eine Gerade zeichnen
+x=cosd(fireAngle)*t + gunposX;
+y=sind(fireAngle)*t + gunposY;
+plot(x,y);
+
+%collision detection 
+% 'in' und 'on' sind wie eine maske für die x und y punkte.
+% um den Einschlag zu detektieren, muss einfach die erste davon verwendet
+% werden, die 1 und nicht 0 ist.
+[in,on]= inpolygon(x,y,terrainshapeX,terrainshapeY); 
+plot(x(in),y(in),'r+') % points inside
+plot(x(on),y(on),'bo') % points on line
+
+impactpos = 100;
+hit = 0;
+end
 
 %% quelle: http://stackoverflow.com/questions/2769249/matlab-how-to-get-the-current-mouse-position-on-a-click-by-using-callbacks
 % set(f,'WindowButtonDownFcn',@mytestcallback)
